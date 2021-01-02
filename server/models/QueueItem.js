30cc518c5b980316bbac8f59931b98695f0679c4
TyreseJin <tyresejin3915@gmail.com>
2021-01-02 13:51:40 -0700

    Queue Item model build

---
+++ server/models/QueueItem.js
@@ -0,0 +1,30 @@
+// REF: https://www.npmjs.com/package/uuid
+// * note: a track uuid contains track index within the playlist, track uri, and track name
+
+const { v4: uuidV4 } = require("uuid");
+
+// ================== //
+//     QUEUE ITEM     //
+// ================== //
+
+class QueueItem {
+  constructor(options = {}) {
+    this.id = options.id || uuidV4();
+    this.track = options.track || {};
+    this.user = options.user || {};
+    this.voters = options.voters || [];
+    this.startTimestamp = options.startTimestamp || null;
+    this.queuedTimestamp = options.queuedTimestamp || Date.now();
+  }
+  toJSON() {
+    return {
+      id: this.id,
+      user: this.user,
+      track: this.track,
+      voters: this.voters,
+      startTimestamp: this.startTimestamp,
+    };
+  }
+}
+
+module.exports = QueueItem;
