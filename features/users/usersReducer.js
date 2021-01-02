89a391ab244746aa3a0be039fe2328fb391a07ea
TyreseJin <tyresejin3915@gmail.com>
2021-01-02 14:00:30 -0700

    Users reducer build

---
+++ features/users/usersReducer.js
@@ -0,0 +1,36 @@
+// REF: https://redux.js.org/tutorials/fundamentals/part-3-state-actions-reducers
+// * note: Reducers are functions that calculate a new state value based on previous state + an action
+
+import { UPDATE_USERS } from "../constants/ActionTypes.js";
+
+// ================== //
+//     USERS SLICE    //
+// ================== //
+
+const initialState = [
+  {
+    id: "something",
+    name: "Jenna",
+  },
+  {
+    id: "something",
+    name: "Nate",
+  },
+  {
+    id: "something",
+    name: "Grant",
+  },
+  {
+    id: "something",
+    name: "Kirsten",
+  },
+];
+
+export default (state, action) => {
+  switch (action.type) {
+    case UPDATE_USERS:
+      return action.data;
+    default:
+      return state ? state : initialState;
+  }
+};
