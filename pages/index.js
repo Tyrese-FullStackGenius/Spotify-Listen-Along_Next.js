3464251cb1e13eb8fa0efb75f424bca2a6ae0dbc
TyreseJin <tyresejin3915@gmail.com>
2021-01-02 14:22:00 -0700

    Added default images for Robot Recommender + users w/o images

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,63 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+// REF: https://react-redux.js.org/using-react-redux/connect-mapdispatch
+// REF: https://react-redux.js.org/using-react-redux/connect-mapstate
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+import Link from "next/link";
+import React from "react";
+import withRedux from "next-redux-wrapper";
+import Layout from "../components/MyLayout.js";
+import { initStore } from "../store/store";
+import { fetchQueue } from "../actions/queueActions";
+import { fetchUsers } from "../actions/usersActions";
+import { fetchPlayingContext } from "../actions/playbackActions";
+import Users from "../components/Users";
+import Queue from "../components/Queue";
+import AddToQueue from "../components/AddToQueue";
+import NowPlaying from "../components/NowPlaying";
+import Devices from "../components/Devices";
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+// ================= //
+//        HOME       //
+// ================= //
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+class Home extends React.Component {
+  static getInitialProps({ req, store, isServer }) {
+    return Promise.all([
+      store.dispatch(fetchQueue()),
+      store.dispatch(fetchUsers()),
+      store.dispatch(fetchPlayingContext()),
+    ]);
+  }
+  render() {
+    return (
+      <Layout>
+        {this.props.playing.track ? (
+          <NowPlaying
+            track={this.props.playing.track}
+            user={this.props.playing.user}
+            position={this.props.playing.position}
+          />
+        ) : null}
+        <div className="app">
+          <div className="queue-container">
+            <Queue items={this.props.queue} session={this.props.session} />
+            {this.props.session.user !== null ? <AddToQueue /> : null}
+            {this.props.session.user !== null ? <Devices /> : null}
+          </div>
+          <div className="users-container">
+            <Users items={this.props.users} />
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+      </Layout>
+    );
+  }
 }
+
+// Extract data with mapStateToProps
+const mapStateToProps = (state) => ({
+  playing: state.playback,
+  queue: state.queue,
+  users: state.users,
+  session: state.session,
+});
+
+export default withRedux(initStore, mapStateToProps, null)(Home);
