24690015e10cbfc32e5073e4b416d906cd3a152f
TyreseJin <tyresejin3915@gmail.com>
2021-01-02 13:59:32 -0700

    Session reducer build

---
+++ features/search/searchReducer.js
@@ -0,0 +1,34 @@
+// REF: https://redux.js.org/tutorials/fundamentals/part-3-state-actions-reducers
+// * note: Reducers are functions that calculate a new state value based on previous state + an action
+
+import {
+  SEARCH_TRACKS,
+  SEARCH_TRACKS_SUCCESS,
+  SEARCH_TRACKS_RESET,
+} from "../constants/ActionTypes.js";
+
+// ================== //
+//    SEARCH SLICE    //
+// ================== //
+
+const initialState = {};
+
+export default (state, action) => {
+  switch (action.type) {
+    case SEARCH_TRACKS:
+      return { query: action.query };
+    case SEARCH_TRACKS_SUCCESS:
+      if (state.query === action.query) {
+        return {
+          query: action.query,
+          results: action.results,
+        };
+      } else {
+        return state;
+      }
+    case SEARCH_TRACKS_RESET:
+      return initialState;
+    default:
+      return state ? state : initialState;
+  }
+};
---
+++ features/session/sessionReducer.js
@@ -0,0 +1,50 @@
+// REF: https://redux.js.org/tutorials/fundamentals/part-3-state-actions-reducers
+// * note: Reducers are functions that calculate a new state value based on previous state + an action
+
+import {
+  LOAD,
+  LOGIN_SUCCESS,
+  UPDATE_TOKEN_SUCCESS,
+  UPDATE_CURRENT_USER,
+} from "../constants/ActionTypes.js";
+
+// ================== //
+//    SESSION SLICE   //
+// ================== //
+
+const initialState = {
+  refresh_token: null,
+  user: null,
+};
+
+export default (state, action) => {
+  switch (action.type) {
+    case LOAD:
+      if (process.browser) {
+        const refreshToken = localStorage.getItem("refreshToken");
+        const accessToken = localStorage.getItem("accessToken");
+        const expiresIn = localStorage.getItem("expiresIn");
+
+        return Object.assign({}, state, {
+          access_token: accessToken,
+          refresh_token: refreshToken,
+          expires_in: expiresIn ? +expiresIn : null,
+        });
+      } else {
+        return state;
+      }
+    case UPDATE_TOKEN_SUCCESS:
+      return Object.assign({}, state, { access_token: action.access_token });
+    case LOGIN_SUCCESS:
+      if (action.refresh_token) {
+        return Object.assign({}, state, {
+          refresh_token: action.refresh_token,
+        });
+      }
+      return state;
+    case UPDATE_CURRENT_USER:
+      return Object.assign({}, state, { user: action.user });
+    default:
+      return state ? state : initialState;
+  }
+};
