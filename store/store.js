03099e744b5fde9fed01072c8d0176185a563f7d
TyreseJin <tyresejin3915@gmail.com>
2021-01-02 13:54:05 -0700

    Initial Redux store build for next.js

---
+++ store/store.js
@@ -0,0 +1,43 @@
+// REF: https://redux.js.org/tutorials/fundamentals/part-3-state-actions-reducers
+// REF: https://redux.js.org/tutorials/fundamentals/part-4-store
+// * note: A Redux store runs the root reducer whenever an action is dispatched
+
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import { reducers } from "../reducers";
+
+import devicesMiddleware from "../middleware/devicesMiddleware.js";
+import loggerMiddleware from "../middleware/loggerMiddleware.js";
+import playbackMiddleware from "../middleware/playbackMiddleware";
+import searchMiddleware from "../middleware/searchMiddleware.js";
+import sessionMiddleware from "../middleware/sessionMiddleware.js";
+import socketMiddlewareDefault from "../middleware/socketMiddleware.js";
+import { socketMiddleware } from "../middleware/socketMiddleware.js";
+
+import { load } from "../actions/sessionActions.js";
+
+// ================== //
+//        STORE       //
+// ================== //
+
+export const initStore = (initialState = {}) => {
+  const store = createStore(
+    reducers(),
+    initialState,
+    applyMiddleware(
+      thunk,
+      // THUNK is a specific kind of Redux function that can contain asynchronous logic
+      // This Redux Thunk middleware modifies the store to let us pass functions into DISPATCH
+      devicesMiddleware,
+      loggerMiddleware,
+      playbackMiddleware,
+      searchMiddleware,
+      sessionMiddleware,
+      socketMiddleware
+    )
+  );
+  socketMiddlewareDefault(store);
+  store.dispatch(load());
+  return store;
+};
