15592b52d4548df6aada9806a623ccc057b70f9b
TyreseJin <tyresejin3915@gmail.com>
2021-01-02 13:57:58 -0700

    Queue reducer build

---
+++ features/queue/queueReducer.js
@@ -0,0 +1,19 @@
+// REF: https://redux.js.org/tutorials/fundamentals/part-3-state-actions-reducers
+// * note: Reducers are functions that calculate a new state value based on previous state + an action
+
+import { UPDATE_QUEUE } from "../constants/ActionTypes.js";
+
+// ================== //
+//     QUEUE SLICE    //
+// ================== //
+
+const initialState = [];
+
+export default (state, action) => {
+  switch (action.type) {
+    case UPDATE_QUEUE:
+      return action.data;
+    default:
+      return state ? state : initialState;
+  }
+};
