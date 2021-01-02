4e7719f55c42ee510c24d16bf245c575d059266c
TyreseJin <tyresejin3915@gmail.com>
2021-01-02 13:27:35 -0700

    Transpiled Users actions from React files

---
+++ features/users/usersActions.js
@@ -0,0 +1,19 @@
+// REF: https://redux.js.org/tutorials/fundamentals/part-2-concepts-data-flow
+// * note: Actions in Redux are plain objects with a TYPE field | Actions are the events that occur in the app based on user input + trigger updates in the state
+import fetch from "isomorphic-unfetch";
+
+import Config from "../config/app.js";
+import * as types from "../constants/ActionTypes.js";
+
+// =============== //
+//     USERS       //
+// =============== //
+export const updateUsers = (users) => ({
+  type: types.UPDATE_USERS,
+  data: users,
+});
+
+export const fetchUsers = () => (dispatch) =>
+  fetch(`${Config.HOST}/api/users`)
+    .then((res) => res.json())
+    .then((res) => dispatch(updateUsers(res)));
