21b688f803471c332de7726608d66aacf92935c7
TyreseJin <tyresejin3915@gmail.com>
2021-01-02 13:56:10 -0700

    Devices reducer build

---
+++ features/devices/devicesReducer.js
@@ -0,0 +1,46 @@
+// REF: https://redux.js.org/tutorials/fundamentals/part-3-state-actions-reducers
+// REF: https://developer.spotify.com/documentation/web-api/guides/using-connect-web-api/
+// * note: Reducers are functions that calculate a new state value based on previous state + an action
+
+import {
+  FETCH_AVAILABLE_DEVICES,
+  FETCH_AVAILABLE_DEVICES_ERROR,
+  FETCH_AVAILABLE_DEVICES_SUCCESS,
+} from "../constants/ActionTypes.js";
+
+// ================== //
+//    DEVICES SLICE   //
+// ================== //
+
+const initialState = {
+  isFetching: false,
+  data: [],
+};
+
+export default (state, action) => {
+  switch (action.type) {
+    case FETCH_AVAILABLE_DEVICES:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case FETCH_AVAILABLE_DEVICES_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        data: action.list,
+      };
+    case FETCH_AVAILABLE_DEVICES_ERROR:
+      return initialState;
+    default:
+      return state ? state : initialState;
+  }
+};
+
+export const getIsFetching = (state) => {
+  return state.isFetching;
+};
+
+export const getDevices = (state) => {
+  return state.data;
+};
