71077b372f7a8c68e98e9605b2b6a7dd70df11a9
TyreseJin <tyresejin3915@gmail.com>
2021-01-02 14:14:14 -0700

    Server.js setup feat. Authorization router and API + Web Socket routing

---
+++ server/views/server.js
@@ -0,0 +1,53 @@
+// REF: https://socket.io/docs/v3/server-initialization/
+// REF: https://developer.spotify.com/documentation/general/guides/authorization-guide/
+
+require("dotenv").config();
+
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+
+const compression = require("compression");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+const auth = require("./authorization.js");
+const api = require("./api.js");
+
+nextApp.prepare().then(() => {
+  app.use(compression());
+  app.use(cookieParser());
+  // *** to-do: shift from ejs to jsx
+
+  app.set("views", __dirname + "/views");
+  app.set("view engine", "ejs");
+
+  // need this to parse our refresh_token
+  app.use(
+    bodyParser.json({
+      limit: 1024,
+    })
+  );
+
+  // Auth Router
+  app.use("/auth", auth);
+
+  // API + Web Socket Router
+  app.use("/api", api(io));
+
+  // *** to-do: 404???
+
+  server.listen(process.env.PORT || 3000, (err) => {
+    if (err) throw err;
+    console.log(
+      `Dude, let's surf the interwebs | Listening on port: ${
+        process.env.PORT || 3000
+      }`
+    );
+  });
+});
