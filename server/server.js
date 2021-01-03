c69384b6424763a8b76b43e726e466809cd7eac8
TyreseJin <tyresejin3915@gmail.com>
2021-01-03 14:57:43 -0700

    Added multi-lang support to my routing in server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 // REF: https://socket.io/docs/v3/server-initialization/
 // REF: https://developer.spotify.com/documentation/general/guides/authorization-guide/
+// REF:
 
 require("dotenv").config();
 
@@ -9,21 +10,49 @@ const io = require("socket.io")(server);
 
 const next = require("next");
 
+const { basename } = require("path");
+const fs = require("fs");
+
 const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
 
 const compression = require("compression");
+const accepts = require("accepts");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 
 const auth = require("./authorization.js");
 const api = require("./api.js");
+const glob = require("glob");
+
+// ===================== //
+//    LOCALE RETRIEVAL   //
+// ===================== //
+
+// Retrieve supported languages by looking for the translations I added to listen-along-app-next/languages
+const languages = glob.sync("./languages/*.json").map((f) => basename(f, json));
+
+const localeDataCache = new Map();
+const getLocaleDataScript = (locale) => {
+  const lang = locale.split("-")[0];
+  // if localeDataCache does not have any languages...
+  if (!localeDataCache.has(lang)) {
+    const localeDataFile = require.resolve(`react-intl/locale-data/${lang}`);
+    const localeDataScript = fs.readFileSync(localeDataFile, "utf8");
+    localeDataCache.set(lang, localeDataScript);
+  }
+
+  return localeDataCache.get(lang);
+};
+
+const getMessages = (locale) => {
+  return require(`../languages/${locale}.json`);
+};
 
 nextApp.prepare().then(() => {
   app.use(compression());
   app.use(cookieParser());
-
   app.set("views", __dirname + "/views");
   // Embedded JavaScript for use with our Spotify Callback render
   app.set("view engine", "ejs");
@@ -42,6 +71,15 @@ nextApp.prepare().then(() => {
   app.use("/api", api(io));
 
   app.get("*", (req, res) => {
+    const accept = accepts(req);
+    const locale = accept.language(languages);
+    req.locale = locale;
+    req.localeDataScript = getLocaleDataScript(locale);
+    req.messages = getMessages(locale);
+    console.log({
+      locale: req.locale,
+      messages: req.messages,
+    });
     return nextHandler(req, res);
   });
 
