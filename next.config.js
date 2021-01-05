574715726965693fabb65e312d62a6facd42a70e
TyreseJin <tyresejin3915@gmail.com>
2021-01-04 18:31:24 -0700

    Fixed issues with Config files by adding a next.config.js file

---
+++ next.config.js
@@ -0,0 +1,25 @@
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const { EnvironmentPlugin } = require("webpack");
+const { ANALYZE } = process.env;
+
+module.exports = {
+  webpack: function (config, { dev }) {
+    if (ANALYZE) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: "server",
+          analyzerPort: 8888,
+          openAnalyzer: true,
+        })
+      );
+    }
+
+    config.plugins.push(new EnvironmentPlugin(["HOST"]));
+
+    if (dev) {
+      return config;
+    }
+
+    return config;
+  },
+};
